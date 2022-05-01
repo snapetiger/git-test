2d9f9e98aec2f0d177f8ac7fd797ba7de0876e9d
Snape <1872594050@qq.com>
2022-05-01 20:11:09 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author snape
+ * @create 2022-05-01 20:04
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
